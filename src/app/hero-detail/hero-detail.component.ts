a16c36bf19ced4fafde39dd9d1d26d54376614b6
Lionel Zhang <lionelzhang123@gmail.com>
2017-12-27 15:57:43 +0800

    fixed Hero route debug...

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common'
 
 import { Hero } from './../shared/models/hero';
 import { HeroService } from './../service/hero/hero.service';
-
-
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
+
 export class HeroDetailComponent implements OnInit {
   @Input()  hero: Hero;
 
@@ -26,9 +26,12 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(){
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .then(data => this.hero = data);
+    // const id = +this.route.snapshot.paramMap.get('id');
+    // this.heroService.getHero(id)
+    //   .then(data => this.hero = data);
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+      .subscribe( hero => this.hero = hero);
   }
 
   goBack(): void {
